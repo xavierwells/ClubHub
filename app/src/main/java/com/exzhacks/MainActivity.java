e6a202e7b0bc128d5810661a4b0b5967dc645019
BaconPlatter <elisabethloeffler@yahoo.com>
2021-10-02 18:28:41 -0500

    Added Firebase functionality, added dummy user

--- app/src/main/java/com/exzhacks/MainActivity.java
+++ app/src/main/java/com/exzhacks/MainActivity.java
@@ -9,19 +9,30 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
+
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class MainActivity extends AppCompatActivity {
 public static FragmentManager fragmentManager;
+//variable for fbfs
+private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
 
         fragmentManager = getSupportFragmentManager();
+        User dummy = new User();
+        dummy.setEmail("email@email.com");
+        dummy.setFirstName("gfgd");
+        dummy.setMajor("compy");
+        dummy.setUserName("xavier");
+        addDataToFirestore(dummy);
 
 
         super.onCreate(savedInstanceState);
@@ -29,6 +40,17 @@ public static FragmentManager fragmentManager;
         BottomNavigationView navView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
+    //Storing a user to Firestore
+    //private void addDataToFirestore user topic = (user) {
+    private void addDataToFirestore(User user){
+
+        //creating collection reference
+        CollectionReference dbTopic = db.collection("Topic");
+        //User u = new User(user);
+
+        dbTopic.add(user);
+    }
 }
---
+++ app/src/main/java/com/exzhacks/User.java
@@ -0,0 +1,79 @@
+package com.exzhacks;
+
+import java.util.ArrayList;
+
+public class User
+{
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String major;
+
+    private ArrayList<String> interests;
+
+    public User()
+    {
+        this.interests = new ArrayList<String>();
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public String getMajor()
+    {
+        return major;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public ArrayList<String> getInterests()
+    {
+        return interests;
+    }
+
+    public void addInterest(String n)
+    {
+        interests.add(n);
+    }
+
+    public void setFirstName(String s)
+    {
+        firstName = s;
+    }
+
+    public void setLastName(String s)
+    {
+        lastName = s;
+    }
+
+    public void setUserName(String s)
+    {
+        userName = s;
+    }
+
+    public void setEmail(String s)
+    {
+        email = s;
+    }
+
+    public void setMajor(String s)
+    {
+        major = s;
+    }
+}
